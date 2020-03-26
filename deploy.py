7cfc37c39537288f5905bdbdb41eb907639e92b4
Ahmed Yousef <ahmedmmy97@gmail.com>
2020-03-26 03:38:35 +0300

    committed!

--- deploy.py
+++ deploy.py
@@ -1,8 +1,27 @@
-from flask import Flask
+import requests
+from bs4 import BeautifulSoup
+import flask
+from flask import request, jsonify
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+session = requests.session()
 
+r = session.get('https://www.worldometers.info/coronavirus/')
+soup = BeautifulSoup(r.text, 'html.parser')
+table = soup.select_one('table').select_one('tbody')
+store = []
 
-@app.route('/')
-def index():
-    return '<h1>hello</h1>'
+for tr in table.select('tr'):
+    country = {'Name': tr.select('td')[0].text.strip(), 'Total': tr.select('td')[1].text.strip(),
+               'New': tr.select('td')[2].text.strip(), 'Death': tr.select('td')[3].text.strip(),
+               'NewDeath': tr.select('td')[4].text.strip(), 'Recovered': tr.select('td')[5].text.strip(),
+               'Active': tr.select('td')[6].text.strip(), 'Serious': tr.select('td')[7].text.strip()}
+    store.append(country)
+
+# A route to return all of the available entries in our catalog.
+@app.route('/api/v1/resources/countries/all', methods=['GET'])
+def api_all():
+    return jsonify(store)
+
+app.run()
