bbfe7855f867c2e6aecde4bf6310f5ecc2a3b5fb
Ahmed Yousef <ahmedmmy97@gmail.com>
2020-09-06 18:59:18 +0300

    updated row indeces to new site format

--- deploy.py
+++ deploy.py
@@ -14,10 +14,10 @@ def scrapData():
     store = []
 
     for tr in table.select('tr'):
-        country = {'Name': tr.select('td')[0].text.strip(), 'Total': tr.select('td')[1].text.strip(),
-                   'New': tr.select('td')[2].text.strip(), 'Death': tr.select('td')[3].text.strip(),
-                   'NewDeath': tr.select('td')[4].text.strip(), 'Recovered': tr.select('td')[5].text.strip(),
-                   'Active': tr.select('td')[6].text.strip(), 'Serious': tr.select('td')[7].text.strip()}
+        country = {'Name': tr.select('td')[1].text.strip(), 'Total': tr.select('td')[2].text.strip(),
+                   'New': tr.select('td')[3].text.strip(), 'Death': tr.select('td')[4].text.strip(),
+                   'NewDeath': tr.select('td')[5].text.strip(), 'Recovered': tr.select('td')[6].text.strip(),
+                   'Active': tr.select('td')[7].text.strip(), 'Serious': tr.select('td')[8].text.strip()}
         store.append(country)
         sortedStore = sorted(store, key=lambda i: int(i['Total'].replace(',', '')), reverse=True)
     return sortedStore
